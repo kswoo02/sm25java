89849dd6d31e71cd1d773a91ff91e5a8394506c5
김선우 <tjsdnrla02@naver.com>
2025-07-01 13:44:53 +0900

    0701

---
+++ day01/src/Main2.java
@@ -0,0 +1,11 @@
+
+public class Main2 {
+    public static void main(String[] args) {
+
+        System.out.printf("Hello a!!");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
