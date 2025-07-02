323c311d0ed30a2a8afc096a1185d2155a3a6426
김선우 <tjsdnrla02@naver.com>
2025-07-02 17:40:13 +0900

    Merge remote-tracking branch 'origin/main'

--- day01/src/array/Array10.java
+++ day01/src/array/Array10.java
@@ -5,16 +5,14 @@ import java.util.Random;
 
 public class Array10 {
     public static void main(String[] args) {
-        int arr[] = new int[5];
-        random rand = new Random();
-
-        for(int i = 0; i < 5; i++){
-            int num = rand.nextInt(bound:10)+1;
-
-            if(i == 0) {
+        int arr [] = new int[5];
+        Random rand = new Random();
+        for(int i=0;i<5;i++){
+            int num = rand.nextInt(10)+1;
+            if(i == 0){
                 arr[i] = num;
             }else{
-                for(int j=0; j<i; j++){
+                for(int j=0;j<i;j++){
                     if(arr[j] == num){
                         i--;
                         break;
@@ -23,8 +21,17 @@ public class Array10 {
                     }
                 }
             }
-
         }
         System.out.println(Arrays.toString(arr));
+
+        int min = arr[0];
+        int max = arr[0];
+        for(int num:arr){
+            if(num < min){ min = num; }
+            if(num > max){ max = num; }
+        }
+        System.out.printf("%d, %d",min, max);
+
+
     }
 }
--- day01/src/array/Array4.java
+++ day01/src/array/Array4.java
@@ -1,31 +1,31 @@
 package array;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class Array4 {
     public static void main(String[] args) {
-        int arr[] = new int[5];
+        // Reference Type...
+        int [] arr1 = new int[5];
+        int arr2 [] = new int[5];
+        int [] arr3 = {1,2,3,4,5};
+
         Random rand = new Random();
 
-        for(int i = 0; i < 5; i++){
-            arr[i] = rand.nextInt(10);
-            System.out.println("arr["+i+"] = "+arr[i]);
-        }
-
-        boolean isDuploicate = false;
-        for(int i = 0; i < 5; i++){
-            if(arr[i] == arr[j]){
-                isDuploicate = true;
-                break;
-            }
-
-            if(isDuploicate){
-                break;
+        for(int i=0;i<5;i++){
+            int num = rand.nextInt(10)+1;
+            if(Arrays.stream(arr1).anyMatch(value -> value == num)){
+                if(i != 0){
+                    i--;
+                }
+            }else{
+                arr1[i] = num;
             }
         }
-
-
-
-
+        System.out.println(Arrays.toString(arr1));
+        System.out.println(Arrays.stream(arr1).sum());
+        System.out.println(Arrays.stream(arr1).average().getAsDouble());
+        System.out.println(Arrays.stream(arr1).max().getAsInt());
+        System.out.println(Arrays.stream(arr1).min().getAsInt());
     }
 }
